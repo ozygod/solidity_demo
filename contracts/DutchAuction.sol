79a3163be9ac06983a88e7d12e3294e22b86ea55
ozygod <ozygod@gmail.com>
2024-12-06 18:34:50 +0800

    - dutch auction demo

---
+++ contracts/DutchAuction.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+interface IERC721 {
+    function transferFrom(address _from, address _to, uint _nftId) external;
+}
+
+contract DutchAuction {
+    uint private constant DURATION = 7 days;
+
+    IERC721 public immutable nft;
+    uint public immutable nftId;
+
+    address payable public immutable seller;
+    uint public immutable startPrice;
+    uint public immutable startAt;
+    uint public immutable expiresAt;
+    uint public immutable discountRate;
+
+    constructor(uint _startPrice, uint _discountRate, address _nft, uint _nftId) {
+        seller = payable(msg.sender);
+        startPrice = _startPrice;
+        discountRate = _discountRate;
+        startAt = block.timestamp;
+        expiresAt = block.timestamp + DURATION;
+
+        require(_startPrice >= DURATION * _discountRate, "start price < discount");
+
+        nft = IERC721(_nft);
+        nftId = _nftId;
+    }
+
+    function getPrice() public view returns (uint) {
+        uint timeElapsed = (block.timestamp - startAt);
+        return (startPrice - discountRate*timeElapsed);
+    }
+
+    function buy() external payable {
+        require(block.timestamp < expiresAt, "auction expired");
+
+        uint price = getPrice();
+        require(msg.value >= price, "balance < price");
+
+        nft.transferFrom(seller, msg.sender, nftId);
+        uint refund = msg.value - price;
+        if (refund > 0) {
+            payable(msg.sender).transfer(refund);
+        }
+        selfdestruct(seller);
+    }
+}
